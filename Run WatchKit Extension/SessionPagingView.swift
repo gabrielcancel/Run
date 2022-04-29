00b724976e5abe4216e93aee22e59cc367a8b410
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-04-29 11:01:48 +0200

    Feat: binding SessionView
    Binding TabView to selection

--- Run WatchKit Extension/SessionPagingView.swift
+++ Run WatchKit Extension/SessionPagingView.swift
@@ -15,7 +15,11 @@ struct SessionPagingView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection){
+            Text("Controls").tag(Tab.controle)
+            Text("Metrics").tag(Tab.metrics)
+            Text("Now Playing").tag(Tab.nowPlaying)
+        }
     }
 }
 
