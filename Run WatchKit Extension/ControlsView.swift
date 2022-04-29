974fa7c3fa6b1c2eca42ff2df4d2c4805bd70887
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-04-29 12:19:32 +0200

    Feat: create ControlsView file

---
+++ Run WatchKit Extension/ControlsView.swift
@@ -0,0 +1,20 @@
+//
+//  ControlsView.swift
+//  Run WatchKit Extension
+//
+//  Created by Cancel Gabriel on 29/04/2022.
+//
+
+import SwiftUI
+
+struct ControlsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlsView()
+    }
+}
