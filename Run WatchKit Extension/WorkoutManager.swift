77c20888734afc7a8d23ab5540f8c627e477878d
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-05-02 15:26:34 +0200

    Feat: create startWorkout func
    Collect activity Type, and load source for HealthStore

--- Run WatchKit Extension/WorkoutManager.swift
+++ Run WatchKit Extension/WorkoutManager.swift
@@ -13,10 +13,33 @@ class WorkoutManager: NSObject, ObservableObject {
     
     let healthStore = HKHealthStore()
     var session: HKWorkoutSession?
-    var builder: HKWorkoutBuilder?
+    var builder: HKLiveWorkoutBuilder?
 
-
+    func startWorkout(workoutType: HKWorkoutActivityType) {
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = workoutType
+        configuration.locationType = .outdoor
+        
+        do {
+            session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
+            builder = session?.associatedWorkoutBuilder()
+        } catch {
+            return
+        }
+        
+        builder?.dataSource = HKLiveWorkoutDataSource(
+            healthStore: healthStore,
+            workoutConfiguration: configuration
+        )
+        
+        let startDate = Date()
+        session?.startActivity(with: startDate)
+        builder?.beginCollection(withStart: startDate) {(success, error) in
+            // Workout started
+        }
+    }
 
 
 }
     
+
