8da6d762f29a8790712d9e9f973a1bf3e0a280a4
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-05-02 15:10:07 +0200

    Feat: create workoutManager
    Implement workoutmanager and link it into RunApp

---
+++ Run WatchKit Extension/WorkoutManager.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutManager.swift
+//  Run WatchKit Extension
+//
+//  Created by Cancel Gabriel on 02/05/2022.
+//
+
+import Foundation
+import HealthKit
+
+class WorkoutManager: NSObject, ObservableObject {
+    
+}
