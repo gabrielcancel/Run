21e9de4afa68969053ec0f50467c256ea03475ac
Gabriel Cancel <60512228+gabrielcancel@users.noreply.github.com>
2022-05-03 14:16:13 +0200

    Feat: update metrics
    Create update function to update all metrics

--- Run WatchKit Extension/WorkoutManager.swift
+++ Run WatchKit Extension/WorkoutManager.swift
@@ -46,6 +46,8 @@ class WorkoutManager: NSObject, ObservableObject {
         )
         
         session?.delegate = self
+        builder?.delegate = self
+
         
         let startDate = Date()
         session?.startActivity(with: startDate)
@@ -99,6 +101,38 @@ class WorkoutManager: NSObject, ObservableObject {
         session?.end()
         showingSummaryView = true
     }
+    
+    // MARK: - Workout Metrics
+    
+    @Published var averageHeartRate: Double = 0
+    @Published var heartRate: Double = 0
+    @Published var activeEnergy: Double = 0
+    @Published var distance: Double = 0
+    
+    func updateForStatistics(_ statistics: HKStatistics?) {
+        guard let statistics = statistics else { return }
+        
+        DispatchQueue.main.async {
+            switch statistics.quantityType {
+            case HKQuantityType.quantityType(forIdentifier: .heartRate):
+                let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+                self.heartRate = statistics.mostRecentQuantity()?.doubleValue(for: heartRateUnit) ?? 0
+                self.averageHeartRate = statistics.averageQuantity()?.doubleValue(for: heartRateUnit) ?? 0
+            case HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned):
+                let energyUnit = HKUnit.kilocalorie()
+                self.activeEnergy = statistics.sumQuantity()?.doubleValue(for: energyUnit) ?? 0
+            case HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning), HKQuantityType.quantityType(forIdentifier: .distanceCycling):
+                let meterUnit = HKUnit.meter()
+                self.distance = statistics.sumQuantity()?.doubleValue(for: meterUnit) ?? 0
+            default:
+                return
+            }
+        }
+    }
+
+    
+    
+    
 }
 
 
@@ -126,4 +160,20 @@ extension WorkoutManager: HKWorkoutSessionDelegate {
     }
 }
     
+
+// MARK: - HKLiveWorkoutBuilderDelegate
+extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
+    func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) {
+    }
+    
+    func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
+        for type in collectedTypes {
+            guard let quantityTypes = type as? HKQuantityType else { return }
+            
+            let statistics = workoutBuilder.statistics(for: quantityTypes)
+            
+            updateForStatistics(statistics)
+        }
+    }
+}
 
