85f6b312f1bc1d1f64a90fac970b6d4adef14715
Gabriel Cancel <gabrielcancel66@gmail.com>
2022-05-20 08:59:28 +0200

    feat: Health Kit autorisation on iPhone

--- Run/ContentView.swift
+++ Run/ContentView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+                    if HKHealthStore.isHealthDataAvailable() {
+                        let healthStore = HKHealthStore()
+                        let workoutType = HKObjectType.workoutType()
+
+                        //reading
+                        let readingTypes = Set([workoutType])
+                        //writing
+                        let writingTypes = Set([ workoutType])
+                        //auth request
+                        healthStore.requestAuthorization(toShare: writingTypes, read: readingTypes) { (success, error) -> Void in
+
+                            if error != nil {
+                                print("error \(String(describing: error?.localizedDescription))")
+                            } else if success {
+                                print("success")
+                                //                    self.startButton.setEnabled(true)
+                            } else if !success {
+                                print("fail")
+
+                                //                    self.startButton.setEnabled(false)
+                            }
+                        }
+                    }
+                }) {
+                    Text("Button")
+                }
     }
 }
 
